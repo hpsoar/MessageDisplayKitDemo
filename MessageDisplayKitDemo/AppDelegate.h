be251930c3e0425c1600119de8b1197e8e98245d
huangpeng <huangpeng@chunyu.me>
2015-10-14 16:43:27 +0800

    init

---
+++ MessageDisplayKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MessageDisplayKitDemo
+//
+//  Created by HuangPeng on 10/14/15.
+//  Copyright © 2015 SpringRain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MessageDisplayKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MessageDisplayKitDemo
+//
+//  Created by HuangPeng on 10/14/15.
+//  Copyright © 2015 SpringRain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
